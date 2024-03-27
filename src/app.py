f51dd78016393ac6fba8c630d64d7180aec74da2
Jerry Yu <jyu565@gmail.com>
2024-03-27 11:41:39 -0700

    Changed theme

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 from dash import Dash, html, dash_table
+import dash_bootstrap_components as dbc
 import pandas as pd
 
 df_2019 = pd.read_csv("data/raw/2019.csv")
 
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    html.Div(children="World Happiness Tracker"),
-    dash_table.DataTable(data=df_2019[["Overall rank", "Country or region", "Score"]].to_dict("records"))
+app.layout = dbc.Container([
+    html.H1(children="World Happiness Tracker"),
+    dash_table.DataTable(data=df_2019[["Overall rank", "Country or region", "Score"]].head(10).to_dict("records"))
 ])
 
 if __name__ == "__main__":
