05d83afc0689d51219b0d662fadd79539cf5d2f2
Jerry Yu <jyu565@gmail.com>
2024-03-27 14:49:15 -0700

    Added country dropdown and filtering for data table

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 
@@ -8,9 +8,43 @@ df_2019 = pd.read_csv("data/raw/2019.csv")
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
-    html.H1(children="World Happiness Tracker"),
-    dash_table.DataTable(data=df_2019[["Overall rank", "Country or region", "Score"]].head(10).to_dict("records"))
+    html.Br(),
+    html.H1("World Happiness Tracker"),
+    html.Label(["Country:"]),
+    dcc.Dropdown(options=sorted(list(df_2019["Country or region"])), placeholder="Select a country...",
+                 id="country-select"),
+    dash_table.DataTable(
+                         id="rank-table")
 ])
+
+@callback(
+    Output("rank-table", "data"),
+    Output("rank-table", "style_data_conditional"),
+    Input("country-select", "value")
+)
+def update_table(country):
+    if country:
+        country_rank = df_2019.loc[df_2019["Country or region"] == country, "Overall rank"].tolist()[0]
+        output_df = df_2019.loc[df_2019["Overall rank"] >= country_rank - 2]
+        output_df = output_df.loc[output_df["Overall rank"] <= country_rank + 2]
+        output_df = output_df[["Overall rank", "Country or region", "Score"]]
+        style = [
+            {
+                "if": {"filter_query": "{{Country or region}} = {}".format(country)},
+                "backgroundColor": "orange",
+                "color": "white"
+            }
+        ]
+    else:
+        output_df = df_2019[["Overall rank", "Country or region", "Score"]].head(10)
+        style = [
+            {
+                "if": {"filter_query": "{{Overall rank}} = {}".format(output_df["Overall rank"].min())},
+                "backgroundColor": "lime",
+            }
+        ]
+
+    return output_df.to_dict("records"), style
 
 if __name__ == "__main__":
     app.run(debug=True)
