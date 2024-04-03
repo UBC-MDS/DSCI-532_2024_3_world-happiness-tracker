91ecc558352d0ffb94b701399e342ac77dbb9a42
Jerry Yu <jyu565@gmail.com>
2024-04-03 13:10:24 -0700

    Generate processed data for dashboard

---
+++ src/data_preprocess.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+df_2019 = pd.read_csv("data/raw/2019.csv").assign(Year=2019)
+df_2018 = pd.read_csv("data/raw/2018.csv").assign(Year=2018)
+df_2017 = pd.read_csv("data/raw/2017.csv").assign(Year=2017)
+df_2016 = pd.read_csv("data/raw/2016.csv").assign(Year=2016)
+df_2015 = pd.read_csv("data/raw/2015.csv").assign(Year=2015)
+
+df_2019.rename(columns={"Country or region": "Country"}, inplace=True)
+df_2018.rename(columns={"Country or region": "Country"}, inplace=True)
+df_2017.rename(columns={"Happiness.Rank": "Overall rank", "Happiness.Score": "Score",
+                        "Economy..GDP.per.Capita.": "GDP per capita", "Family": "Social support",
+                        "Health..Life.Expectancy.": "Healthy life expectancy", "Freedom": "Freedom to make life choices",
+                        "Trust..Government.Corruption.": "Perceptions of corruption"},
+                        inplace=True)
+df_2016.rename(columns={"Happiness Rank": "Overall rank", "Happiness Score": "Score", "Economy (GDP per Capita)": "GDP per capita",
+                        "Family": "Social support", "Health (Life Expectancy)": "Healthy life expectancy",
+                        "Freedom": "Freedom to make life choices", "Trust (Government Corruption)": "Perceptions of corruption"},
+                        inplace=True)
+df_2015.rename(columns={"Happiness Rank": "Overall rank", "Happiness Score": "Score", "Economy (GDP per Capita)": "GDP per capita",
+                        "Family": "Social support", "Health (Life Expectancy)": "Healthy life expectancy",
+                        "Freedom": "Freedom to make life choices", "Trust (Government Corruption)": "Perceptions of corruption"},
+                        inplace=True)
+
+df_2019 = df_2019[["Overall rank", "Country", "Score", "GDP per capita", "Social support", "Healthy life expectancy",
+                    "Freedom to make life choices", "Generosity", "Perceptions of corruption", "Year"]]
+df_2018 = df_2018[["Overall rank", "Country", "Score", "GDP per capita", "Social support", "Healthy life expectancy",
+                    "Freedom to make life choices", "Generosity", "Perceptions of corruption", "Year"]]
+df_2017 = df_2017[["Overall rank", "Country", "Score", "GDP per capita", "Social support", "Healthy life expectancy",
+                    "Freedom to make life choices", "Generosity", "Perceptions of corruption", "Year"]]
+df_2016 = df_2016[["Overall rank", "Country", "Score", "GDP per capita", "Social support", "Healthy life expectancy",
+                    "Freedom to make life choices", "Generosity", "Perceptions of corruption", "Year"]]
+df_2015 = df_2015[["Overall rank", "Country", "Score", "GDP per capita", "Social support", "Healthy life expectancy",
+                    "Freedom to make life choices", "Generosity", "Perceptions of corruption", "Year"]]
+
+countries_2019 = set(df_2019["Country"].tolist())
+countries_2018 = set(df_2018["Country"].tolist())
+countries_2017 = set(df_2017["Country"].tolist())
+countries_2016 = set(df_2016["Country"].tolist())
+countries_2015 = set(df_2015["Country"].tolist())
+select_countries = countries_2019.intersection(countries_2018).intersection(countries_2017)\
+    .intersection(countries_2016).intersection(countries_2015)
+
+df_all = pd.concat([df_2019, df_2018, df_2017, df_2016, df_2015])
+df_all["Score"] = df_all["Score"].round(3)
+
+factors = ["GDP per capita", "Social support", "Healthy life expectancy",
+           "Freedom to make life choices", "Generosity", "Perceptions of corruption"]
+df_2019["sum"] = df_2019[factors].sum(axis=1)
+df_2019[factors] = df_2019[factors].div(df_2019["sum"], axis=0)
+df_2019.drop(columns=["sum"], inplace=True)
+
+df_all["sum"] = df_all[factors].sum(axis=1)
+df_all[factors] = df_all[factors].div(df_all["sum"], axis=0)
+df_all.drop(columns=["sum"], inplace=True)
+df_all = df_all.loc[df_all["Country"].isin(select_countries)]
+
+
+df_all.to_csv("data/processed/app_data.csv")
