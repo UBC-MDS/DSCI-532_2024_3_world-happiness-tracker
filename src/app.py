bdfcb8748c53a68946840efe3a2e7b23da9095c1
Jerry Yu <74574486+jy1909@users.noreply.github.com>
2024-04-04 10:27:32 -0700

    Key metrics cards (#36)
    Closes #28

--- src/app.py
+++ src/app.py
@@ -17,36 +17,24 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Components
 card_happiest = dbc.Card(
-    [dbc.CardBody([
-        html.P("Happiest Country"),
-        html.Br(),
-        html.H4("[  country_name (score)  ]")
-    ])],
     color="#ececec",
-    style={"border": 0, "height": 200}
+    style={"border": 0, "height": 200},
+    id="card-happiest"
 )
-card_median = dbc.Card([dbc.CardBody([
-        html.P("Median Happiness Score"),
-        html.Br(),
-        html.H4("[  score  ]")
-    ])],
+card_median = dbc.Card(
     color="#ececec",
-    style={"border": 0, "height": 200}
+    style={"border": 0, "height": 200},
+    id="card-median"
 )
-card_unhappiest = dbc.Card([dbc.CardBody([
-        html.P("Unhappiest Country"),
-        html.Br(),
-        html.H4("[  country_name (score)  ]")
-    ])],
+card_unhappiest = dbc.Card(
     color="#ececec",
-    style={"border": 0, "height": 200}
+    style={"border": 0, "height": 200},
+    id="card-unhappiest"
 )
-card_range = dbc.Card([dbc.CardBody([
-        html.P("Score Difference between Happiest and Unhappiest Country"),
-        html.H4("[  range  ]")
-    ])],
+card_range = dbc.Card(
     color="#ececec",
-    style={"border": 0, "height": 200}
+    style={"border": 0, "height": 200},
+    id="card-range"
 )
 country1_dropdown = dcc.Dropdown(options=sorted(list(set(df_all["Country"]))),
                                  placeholder="Select Country 1...",
@@ -102,6 +90,80 @@ app.layout = dbc.Container([
 
 
 # Server side callbacks/reactivity
+@callback(
+    Output("card-happiest", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    max_score = df_card["Score"].max()
+    happiest_country = df_card.loc[df_card["Score"] == max_score, "Country"].reset_index(drop=True)[0]
+
+    card_body = dbc.CardBody([
+        html.P(f"Happiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{happiest_country}", style={"text-align": "center"}),
+        html.H5(f"({max_score})", style={"text-align": "center", "font-style": "italic"})
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-median", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    median_score = df_card["Score"].median()
+
+    card_body = dbc.CardBody([
+        html.P(f"Median Happiness Score ({year})"),
+        html.Br(style={"line-height": "250%"}),
+        html.H3(f"{median_score}", style={"text-align": "center"}),
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-unhappiest", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    min_score = df_card["Score"].min()
+    unhappiest_country = df_card.loc[df_card["Score"] == min_score, "Country"].reset_index(drop=True)[0]
+
+    card_body = dbc.CardBody([
+        html.P(f"Unhappiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{unhappiest_country}", style={"text-align": "center"}),
+        html.H5(f"({min_score})", style={"text-align": "center", "font-style": "italic"})
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-range", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    max_score = df_card["Score"].max()
+    min_score = df_card["Score"].min()
+    score_range = round(max_score - min_score, 3)
+
+    card_body = dbc.CardBody([
+        html.P(f"Score Difference between Happiest and Unhappiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{score_range}", style={"text-align": "center"}),
+    ])
+
+    return card_body
+
+
 @callback(
     Output("world-map", "figure"),
     Input("country1-select", "value"),
