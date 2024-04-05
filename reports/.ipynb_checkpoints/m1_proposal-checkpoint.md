f17ea6e7fe9b6f9d403332a7aae3021e0eebb0fd
Hongyang Zhang <alexzhang_0825@live.com>
2024-04-04 21:43:47 -0700

    first draft of the reflection document complete

---
+++ reports/.ipynb_checkpoints/m1_proposal-checkpoint.md
@@ -0,0 +1,51 @@
+# World Happiness Tracker App Dashboard Proposal
+
+## Section 1: Motivation and Purpose
+
+Our Role: Developer of Teaching Tools
+
+Target Audience: Educational Dashboard for Students  
+
+Exploring various methods to learn about happiness in the world is commonplace within educational contexts, encompassing fields such as psychology, sociology, and economics. We inhabit a world where comprehending and comparing global happiness is increasingly significant. With the expanding divide between technology and conventional education, this dashboard seeks to bridge that gap by integrating technological tools into educational settings, thereby enhancing students understanding of happiness around the globe.  
+
+The ability to visually interact with this dashboard enables students to see different countries' happiness scores, as well as the factors contributing to these specific scores, through a variety of filters. Students will also gain an understanding of how countries' happiness scores compare to the global average and how these scores have evolved over recent years.
+
+## Section 2: Description of Data
+
+For our visualization, we will use a dataset spanning from 2015 to 2019, comprising approximately 156 rows (countries) for each year. This brings the total to 782 rows. The dataset includes various columns that indicate a country's happiness ranking and score, as well as the factors contributing to this score. The columns that are to be used in this dashboard include:
+
+1. `Country`: String values that will appear five times, once for each year, indicating the name of the country in the dataset.
+
+2. `Overall rank` (also called `Happiness Rank` or `Happiness.Rank` in certain years): Integer values ranging from 1 to 158, representing the country’s happiness ranking globally. Each ranking will (typically) appear five times, as the countries are ranked once every year.
+
+3. `Score` (also called `Happiness Score` or `Happiness.Score` in certain years): Continuous values that typically range between 7.8 and 2.8, with higher scores indicating greater happiness according to the data.  
+
+4. The following columns (contributing factors to happiness): `GDP per capita` (also known as `Economy..GDP.per.Capita.` or `Economy (GDP per Capita)` in certain years), `Social support` (also known as `Family` in certain years), `Healthy life expectancy` (also known as `Health..Life.Expectancy.` or `Health (Life Expectancy)` in certain years), `Freedom to make life choices` (also known as `Freedom` in certain years), `Generosity`, and `Perceptions of corruption` (also known as `Trust..Government.Corruption.` or `Trust (Government Corruption)` in certain years) are continuous variables. They detail the extent to which these factors contribute to a country's happiness score. These variables can be interpreted as coefficients and range between 0 and 2.
+
+Utilizing this data, we will create certain columns such as a `year` column and a normalized version of each contribution factor to happiness above to provide additional, more interpretable data points. This will aid in better understanding the impact of various factors on a country's Happiness Score.
+
+## Section 3: Research Questions and Usage Scenarios
+
+Geography and economy classes in high school are mostly preliminary. They do not touch on really sophisticated topics and are only there to build the foundation for any future learning. A simple tool displaying world happiness level can let students understand the distributions of happiness across the world, how some of the happiest and unhappiest countries differ, and also inspect some of the underlying factors that influence these levels.
+
+Our target audiences are members of high school geography/economy classes (teachers and students). John is a high school student learning about economics. He would like to learn about happiness around the world and what factors influence this. He is also interested in how happiness changes across the years.
+
+Using our tool, he is able to find the happiness level for each country/region across the world over time and compare two against each other in terms of their happiness level, ranking, and how some factors influence their levels such as economy, freedom, trust in government, etc. He can also use our filters to investigate deeper into specific countries and years of interest.
+
+Hypothetically, John finds that the economy is a major factor contributing to the happiness level, which likely makes sense considering that ultimately economy is the most major tie with quality of life for people around the world. He can then perhaps probe further into the economies of the happiest and unhappiest countries in the world and see how they differ.
+
+## Section 4: App Sketch and Brief Description
+
+![sketch](../img/sketch.png)
+
+The World Happiness Tracker App allows users to explore the Happiness Score of different countries.
+
+At the top of the page, the dashboard presents some global key numbers to the user. These numbers aim to give a user some reference points for the interpretation of further information.
+
+Below the key numbers block, the app presents the center piece of the app which is the world map that encodes the happiness score for each country with the help of a color scale. The default of the world map is to show the happiness scores for all countries for the most recent year. As the user chooses a particular year, the map will be adjusted to reflect the user's selection.
+
+The block below the map consists of three parts that allow more insights into the happiness of the selected countries of the selected year (and by default, all countries of the current year are selected). The ranking displays the rank of each of the selected countries out of all the countries.
+
+The line plot visualizes the happiness over time of the two countries A and B over the years and highlights the year selected. By default, with no countries or year selected, the a line of the global average will be shown with no highlights.
+
+Finally, the bar chart breaks down the happiness score into its components and shows how much individual components contributed to the overall score.
---
+++ src/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,304 @@
+from dash import Dash, html, dash_table, dcc, Input, Output, callback
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+
+df_all = pd.read_csv("data/processed/app_data.csv")
+factors = ["GDP per capita", "Social support", "Healthy life expectancy",
+           "Freedom to make life choices", "Generosity", "Perceptions of corruption"]
+
+
+# Initialize the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+# Components
+card_happiest = dbc.Card(
+    color="#ececec",
+    style={"border": 0, "height": 200},
+    id="card-happiest"
+)
+card_median = dbc.Card(
+    color="#ececec",
+    style={"border": 0, "height": 200},
+    id="card-median"
+)
+card_unhappiest = dbc.Card(
+    color="#ececec",
+    style={"border": 0, "height": 200},
+    id="card-unhappiest"
+)
+card_range = dbc.Card(
+    color="#ececec",
+    style={"border": 0, "height": 200},
+    id="card-range"
+)
+country1_dropdown = dcc.Dropdown(options=sorted(list(set(df_all["Country"]))),
+                                 placeholder="Select Country 1...",
+                                 id="country1-select")
+country2_dropdown = dcc.Dropdown(options=sorted(list(set(df_all["Country"]))),
+                                 placeholder="Select Country 2...",
+                                 id="country2-select")
+year_slider = dcc.Slider(min=2015, max=2019, value=2019,
+                         marks={i: "{}".format(i) for i in range(2015,2020)},
+                         step=1, id="year-select")
+world_map = dcc.Graph(id="world-map")
+table_title = html.P("Country Rankings")
+rank_table = dash_table.DataTable(id="rank-table")
+line_chart = dcc.Graph(id="line-chart")
+factors_graph = dcc.Graph(id="factors-graph")
+
+
+# Layout
+app.layout = dbc.Container([
+    html.Br(),
+    html.H1("World Happiness Tracker"),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(html.Label(["Country 1"])),
+        dbc.Col(html.Label(["Country 2"])),
+        dbc.Col(html.Label(["Year"]))
+    ]),
+    dbc.Row([
+        dbc.Col(country1_dropdown),
+        dbc.Col(country2_dropdown),
+        dbc.Col(year_slider)
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(card_happiest),
+        dbc.Col(card_median),
+        dbc.Col(card_unhappiest),
+        dbc.Col(card_range)
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(world_map, width=8),
+        dbc.Col([dbc.Row(table_title),
+                 dbc.Row(rank_table)])
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(line_chart),
+        dbc.Col(factors_graph)
+    ]),
+    html.Br(),
+])
+
+
+# Server side callbacks/reactivity
+@callback(
+    Output("card-happiest", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    max_score = df_card["Score"].max()
+    happiest_country = df_card.loc[df_card["Score"] == max_score, "Country"].reset_index(drop=True)[0]
+
+    card_body = dbc.CardBody([
+        html.P(f"Happiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{happiest_country}", style={"text-align": "center"}),
+        html.H5(f"({max_score})", style={"text-align": "center", "font-style": "italic"})
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-median", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    median_score = df_card["Score"].median()
+
+    card_body = dbc.CardBody([
+        html.P(f"Median Happiness Score ({year})"),
+        html.Br(style={"line-height": "250%"}),
+        html.H3(f"{median_score}", style={"text-align": "center"}),
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-unhappiest", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    min_score = df_card["Score"].min()
+    unhappiest_country = df_card.loc[df_card["Score"] == min_score, "Country"].reset_index(drop=True)[0]
+
+    card_body = dbc.CardBody([
+        html.P(f"Unhappiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{unhappiest_country}", style={"text-align": "center"}),
+        html.H5(f"({min_score})", style={"text-align": "center", "font-style": "italic"})
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("card-range", "children"),
+    Input("year-select", "value")
+)
+def update_card_happiest(year):
+    df_card = df_all.loc[df_all["Year"] == year]
+    max_score = df_card["Score"].max()
+    min_score = df_card["Score"].min()
+    score_range = round(max_score - min_score, 3)
+
+    card_body = dbc.CardBody([
+        html.P(f"Score Difference between Happiest and Unhappiest Country ({year})"),
+        html.Br(),
+        html.H3(f"{score_range}", style={"text-align": "center"}),
+    ])
+
+    return card_body
+
+
+@callback(
+    Output("world-map", "figure"),
+    Input("country1-select", "value"),
+    Input("year-select", "value")
+)
+def update_map(country1, year):
+    map_df = df_all.loc[df_all["Year"] == year]
+    fig = px.choropleth(map_df, locations="Country", color="Score", locationmode="country names",
+                        color_continuous_scale=px.colors.sequential.Blues,
+                        title="World Map of Happiness Scores")
+    fig.update_layout(margin=dict(l=0, r=80, b=0, t=50))
+    
+    return fig
+
+
+@callback(
+    Output("rank-table", "data"),
+    Output("rank-table", "style_data_conditional"),
+    Input("country1-select", "value"),
+    Input("country2-select", "value"),
+    Input("year-select", "value")
+)
+def update_table(country1, country2, year):
+    output_df = df_all.loc[df_all["Year"] == year]
+    if country1 and country2:
+        # pasting code for no countries selected for now; change code in this condition
+        output_df = output_df[["Overall rank", "Country", "Score"]].head(10)
+        style = [
+            {
+                "if": {"filter_query": "{{Overall rank}} = {}".format(output_df["Overall rank"].min())},
+                "backgroundColor": "lime",
+            }
+        ]
+    elif country1:
+        # pasting code for no countries selected for now; change code in this condition
+        country_rank = output_df.loc[output_df["Country"] == country1, "Overall rank"].tolist()[0]
+        output_df = output_df.loc[output_df["Overall rank"] >= country_rank - 2]
+        output_df = output_df.loc[output_df["Overall rank"] <= country_rank + 2]
+        output_df = output_df[["Overall rank", "Country", "Score"]]
+        style = [
+            {
+                "if": {"filter_query": "{{Overall rank}} = {}".format(country_rank)},
+                "backgroundColor": "orange",
+                "color": "white"
+            }
+        ]
+    elif country2:
+        # pasting code for no countries selected for now; change code in this condition
+        output_df = output_df[["Overall rank", "Country", "Score"]].head(10)
+        style = [
+            {
+                "if": {"filter_query": "{{Overall rank}} = {}".format(output_df["Overall rank"].min())},
+                "backgroundColor": "lime",
+            }
+        ]
+    else:
+        output_df = output_df[["Overall rank", "Country", "Score"]].head(10)
+        style = [
+            {
+                "if": {"filter_query": "{{Overall rank}} = {}".format(output_df["Overall rank"].min())},
+                "backgroundColor": "lime",
+            }
+        ]
+
+    return output_df.to_dict("records"), style
+
+
+@callback(
+    Output("line-chart", "figure"),
+    Input("country1-select", "value"),
+    Input("country2-select", "value"),
+    Input("year-select", "value")
+)
+def update_linechart(country1, country2, year):
+    if country1 and country2:
+        # pasting code for no countries selected for now; change code in this condition
+        grouped_df = pd.DataFrame(df_all.groupby(["Year"])["Score"].mean()).reset_index()
+        y_points = grouped_df.loc[grouped_df["Year"] == year, "Score"]
+        fig = px.line(grouped_df, x="Year", y="Score", markers=True, title="Happiness Score over the Years")
+        fig.add_trace(go.Scatter(x=[year], y=y_points, mode = "markers", name="Selected Year",
+                                 marker_size = 15))
+    elif country1:
+        # pasting code for no countries selected for now; change code in this condition
+        grouped_df = pd.DataFrame(df_all.groupby(["Year"])["Score"].mean()).reset_index()
+        y_points = grouped_df.loc[grouped_df["Year"] == year, "Score"]
+        fig = px.line(grouped_df, x="Year", y="Score", markers=True, title="Happiness Score over the Years")
+        fig.add_trace(go.Scatter(x=[year], y=y_points, mode = "markers", name="Selected Year",
+                                 marker_size = 15))
+    elif country2:
+        # pasting code for no countries selected for now; change code in this condition
+        grouped_df = pd.DataFrame(df_all.groupby(["Year"])["Score"].mean()).reset_index()
+        y_points = grouped_df.loc[grouped_df["Year"] == year, "Score"]
+        fig = px.line(grouped_df, x="Year", y="Score", markers=True, title="Happiness Score over the Years")
+        fig.add_trace(go.Scatter(x=[year], y=y_points, mode = "markers", name="Selected Year",
+                                 marker_size = 15))
+    else:
+        grouped_df = pd.DataFrame(df_all.groupby(["Year"])["Score"].mean()).reset_index()
+        y_points = grouped_df.loc[grouped_df["Year"] == year, "Score"]
+        fig = px.line(grouped_df, x="Year", y="Score", markers=True, title="Happiness Score over the Years")
+        fig.add_trace(go.Scatter(x=[year], y=y_points, mode = "markers", name="Selected Year",
+                                 marker_size = 15))
+    
+    return fig
+
+
+@callback(
+    Output("factors-graph", "figure"),
+    Input("country1-select", "value"),
+    Input("country2-select", "value"),
+    Input("year-select", "value")
+)
+def update_contributing_factors(country1, country2, year):
+    factors_df = df_all.loc[df_all["Year"] == year]
+
+    if country1 and country2:
+        # does the same thing as if no countries are selected for now;
+        #    change code in this condition
+        pass
+    elif country1:
+        factors_df = factors_df.loc[factors_df["Country"] == country1]
+    elif country2:
+        # does the same thing as if no countries are selected for now;
+        #    change code in this condition
+        pass
+
+    factors_df = factors_df[factors].melt(value_vars=factors, var_name="Factors", value_name="Proportion")
+
+    fig = px.histogram(factors_df, x="Proportion", y="Factors", histfunc="avg")
+    fig.update_layout(yaxis={"categoryorder": "mean ascending"},
+                      xaxis_title="Proportion of Contribution", yaxis_title="Factors",
+                      title="Factors Contributing to Happiness Index")
+
+    return fig
+
+
+# Run the app/dashboard
+if __name__ == "__main__":
+    app.run(debug=True)
