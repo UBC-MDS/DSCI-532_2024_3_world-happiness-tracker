5a6cdacd8eff6043cf915e318bb65aca47e67eed
Jerry Yu <jyu565@gmail.com>
2024-04-17 22:02:16 -0700

    Added caching to callbacks

--- src/callbacks/navbar.py
+++ src/callbacks/navbar.py
@@ -1,4 +1,5 @@
 from dash import Input, Output, State, callback
+import functools
 
 
 @callback(
@@ -6,6 +7,7 @@ from dash import Input, Output, State, callback
     Input("open-about", "n_clicks"),
     State("about-text", "style"),
 )
+@functools.lru_cache()
 def toggle_about(n, about_style):
     if n:
         if about_style and about_style.get('display') == 'none':
