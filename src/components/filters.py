947cedd8f494a28833d4c0aad90ef2cedb45fd11
Michelle <143568175+mishelly-h@users.noreply.github.com>
2024-04-19 15:33:52 -0700

    Change year slider colour (#103)
    Closes #97

--- src/components/filters.py
+++ src/components/filters.py
@@ -11,4 +11,5 @@ country2_dropdown = dcc.Dropdown(options=sorted(list(set(happiness_data["Country
                                  id="country2-select")
 year_slider = dcc.Slider(min=2015, max=2019, value=2019,
                          marks={i: "{}".format(i) for i in range(2015,2020)},
-                         step=1, included=False, id="year-select")
+                         step=1, included=False, id="year-select",
+                         className='slider-class')
